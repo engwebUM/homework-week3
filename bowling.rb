8078988fa55353c70c0a77e7834f1d4fc4817fb8
jmtalves <alvesjose13@gmail.com>
2015-03-06 23:13:56 +0000

    clean some code

--- bowling.rb
+++ bowling.rb
@@ -1,48 +1,44 @@
 class Bowling
   def initialize 
-
   	@score=Array.new(10) { Hash.new }
     @gamescore=Array.new
     @spare=0
-
   end
+
+
   def roll(pins)
-    # roll the desired number of pins
-    for i in 0..@score.length-1
-
-      if i<9
-
-      if (@score[i][0] == nil)
-        @score[i][0]=pins
-
-        
-        break
-      elsif (@score[i][1] == nil && @score[i][0]!=10)
-        @score[i][1]=pins
-          break
-      end
-
-     elsif @score[i][0] == nil && i==9
-
-          @score[9][0]=pins
-        elsif @score[i][1] == nil && i==9
-              @score[9][1]=pins
-        elsif @score[i][2] == nil && i==9
-              @score[9][2]=pins
-            end
+    raise unless pins.is_a?(Numeric)
+    if(pins>10 || pins <0)
+      raise ArgumentError, 'Number pins incorrect'
     end
 
+    for i in 0..@score.length-1
+      if i<9
+        if (@score[i][0] == nil)
+          @score[i][0]=pins
+          break
+        elsif (@score[i][1] == nil && @score[i][0]!=10)
+          if @score[i][0]+pins>10
+            raise ArgumentError, 'Number pins too large'
+          end
+          @score[i][1]=pins
+          break
+        end
+      elsif @score[i][0] == nil && i==9
+          @score[9][0]=pins
+      elsif @score[i][1] == nil && i==9
+          @score[9][1]=pins
+      elsif @score[i][2] == nil && i==9
+          @score[9][2]=pins
+      end
+    end
   end
-
 
 
   def score
-    # return the current score
-
     @strike=0
     for i in 0..@score.length-1
       if (@score[i][0] != nil && @score[i][0]!=10)
-
         callSpare(i)
         @gamescore << @score[i][0]
       elsif @score[i][0]!=nil && @score[i][0]==10
@@ -51,26 +47,21 @@ class Bowling
         Strike(i)
       end
       if @strike==0
-            if (@score[i][1] != nil && @score[i][1]+@score[i][0]==10)
-              @spare=1
-              @gamescore[positionElement]=@score[i][0]+@score[i][1]
-              
-            elsif (@score[i][1] != nil && @score[i][1]+@score[i][0]!=10)
-                @gamescore[positionElement]=@gamescore[positionElement]+@score[i][1]
-            end
+         if (@score[i][1] != nil && @score[i][1]+@score[i][0]==10)
+           @spare=1
+           @gamescore[positionElement]=@score[i][0]+@score[i][1]
+         elsif (@score[i][1] != nil && @score[i][1]+@score[i][0]!=10)
+           @gamescore[positionElement]=@gamescore[positionElement]+@score[i][1]
+         end
       else
         @strike=0
       end
-  
     end
-
     if (@score[9][2]!=nil) && @score[9][0]+@score[9][1]==10
       Spare(@score[9][2])
-          @spare=0
+      @spare=0
     end
-
- 
-  return @gamescore.inject{|sum,x| sum + x }
+    return @gamescore.inject{|sum,x| sum + x }
   end
 
   private
@@ -106,9 +97,6 @@ class Bowling
         @gamescore<<@score[9][0]+@score[9][1]+@score[9][2]
     end
   end
-
-
-  # Add code as needed
 end
 
 
