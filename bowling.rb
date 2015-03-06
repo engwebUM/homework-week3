80767a99fffd955dd0f5275ba00ac99262bd99e1
zem3 <zemm10a@gmail.com>
2015-03-06 11:08:02 +0000

    working solution

--- bowling.rb
+++ bowling.rb
@@ -4,13 +4,60 @@
 # SOB PENA DE NOS SER ATRIBUÍDA A CLASSIFICAÇÃO 0*.
 
 class Bowling
+  def initialize
+    @score = 0
+    @current_frame = 1
+    @current_frame_score = nil
+    @bonus = [0, 0]
+  end
+
   def roll(pins)
-    # roll the desired number of pins
+    if @current_frame > 11 || @bonus[0] == nil
+      return
+    end
+
+    @score += pins * (1 + @bonus[0])
+    @bonus.shift
+
+    if @current_frame < 10
+      @bonus.push(0)
+    end
+
+    case
+    when pins==10 && (@current_frame_score == nil || @current_frame == 10 && @current_frame_score == 10)
+      if @bonus[0] != nil
+        @bonus[0] += 1
+      end
+      if @bonus[1] != nil
+        @bonus[1] += 1
+      end
+      if @current_frame == 10 && @current_frame_score == nil
+        @bonus.push(0)
+      end
+    when @current_frame_score != nil && @current_frame_score + pins == 10 && @current_frame_score != 10
+      if @bonus[0] != nil
+        @bonus[0] += 1
+      end
+      if @current_frame == 10
+        @bonus.push(0)
+      end
+    end
+
+    if @current_frame_score != nil || pins == 10
+      next_frame
+    else
+      @current_frame_score = pins
+    end
   end
 
   def score
-    # return the current score
+    @score
   end
 
-  # Add code as needed
+  private
+
+  def next_frame
+    @current_frame_score = nil
+    @current_frame += 1
+  end
 end
--- bowling_spec.rb
+++ bowling_spec.rb
@@ -12,16 +12,15 @@ RSpec.describe Bowling do
   end
 
   context '#roll' do
-    it "even number of rolls" do
-      @b.roll(1)
-      @b.roll(1)
-      expect(@b.score).to eq(2)
-    end
-
     it "odd number of rolls" do
       (1..3).each { @b.roll(1) }
       expect(@b.score).to eq(3)
     end  
+
+    it "even number of rolls" do
+      (1..4).each { @b.roll(1) }
+      expect(@b.score).to eq(4)
+    end
 
     it "spare" do
       (1..3).each { @b.roll(5) }
@@ -33,6 +32,27 @@ RSpec.describe Bowling do
       @b.roll(1)
       @b.roll(1)
       expect(@b.score).to eq(14)
+    end
+  end
+
+  context '#score' do
+    it "the score is initially 0" do
+      expect(@b.score).to eq(0)
+    end
+
+    it "given example" do
+      [1, 4, 4, 5, 6, 4, 5, 5, 10, 0, 1, 7, 3, 6, 4, 10, 2, 8, 6].each { |x| @b.roll(x) }
+      expect(@b.score).to eq(133)
+    end
+
+    it "minimum score" do
+      (1..10).each { @b.roll(0) }
+      expect(@b.score).to eq(0)
+    end
+
+    it "maximum score" do
+      (1..12).each { @b.roll(10) }
+      expect(@b.score).to eq(300)
     end
 
     context "game ends" do
@@ -56,27 +76,6 @@ RSpec.describe Bowling do
         (1..12).each { @b.roll(1) }
         expect(@b.score).to eq(score)
       end
-    end
-  end
-
-  context '#score' do
-    it "the score is initially 0" do
-      expect(@b.score).to eq(0)
-    end
-
-    it "example scenario" do
-      [1, 4, 4, 5, 6, 4, 5, 5, 10, 0, 1, 7, 3, 6, 4, 10, 2, 8, 6].each { |x| @b.roll(x) }
-      expect(@b.score).to eq(133)
-    end
-
-    it "minimum score" do
-      (1..10).each { @b.roll(0) }
-      expect(@b.score).to eq(0)
-    end
-
-    it "maximum score" do
-      (1..12).each { @b.roll(10) }
-      expect(@b.score).to eq(300)
     end
   end
 end
