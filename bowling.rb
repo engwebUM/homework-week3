ab5594c1b2cfdf2527abe080a26da116329a1daf
filipadeus <filipadeus6@gmail.com>
2015-03-08 19:02:05 +0000

    Exercises bowling.rb and bowling_spec.rb

--- bowling.rb
+++ bowling.rb
@@ -1,11 +1,72 @@
 class Bowling
+  attr_reader :frames, :current_frame, :score, :count_frames, :count_frame_balls
+
+  def initialize
+    @frames = []
+    @current_frame = []
+    @score = 0
+    @count_frames = 0
+    @count_frame_balls = 0
+  end
+
+  def strike (frame_index)
+    strike = []
+    strike = @frames[frame_index + 1]
+    return 10 + strike[0] + strike[1]
+  end
+
+  def spare (frame_index)
+    spare = []
+    spare = @frames[frame_index + 1]
+    return 10 + spare[0]
+  end
+
+
   def roll(pins)
-    # roll the desired number of pins
+    if pins == 10 && @current_frame.length == 0
+      @current_frame.push(pins)
+      @current_frame.push(0)
+      @frames.push(current_frame)
+      @count_frames += 1
+      @current_frame = []
+      @count_frame_balls = 0
+
+    elsif @current_frame.length == 1 && @count_frames != 10
+      @current_frame.push(pins)
+      @frames[count_frames] = @current_frame
+      @current_frame = []
+      @count_frame_balls = 0
+      @count_frames += 1
+    else
+      @current_frame.push(pins)
+      @frames.push(current_frame)
+      @count_frame_balls += 1
+    end
+
   end
 
   def score
-    # return the current score
+    index = 0
+    score = 0
+    aux_current_frame = []
+    @frames.each_index do |i|
+      aux_current_frame = @frames[i]
+      #strike
+      if aux_current_frame[0] == 10
+        score += strike (i)
+        index +=1
+        #sparke
+      elsif aux_current_frame.length== 2 && (aux_current_frame[0] + aux_current_frame[1])  == 10
+          score += spare (i)
+
+      else
+        aux_current_frame.each do |x|
+          score += x
+        end
+      end
+    end
+  return score
   end
 
-  # Add code as needed
+
 end
--- bowling_spec.rb
+++ bowling_spec.rb
@@ -2,11 +2,54 @@ require_relative './bowling'
 require 'rspec'
 
 RSpec.describe Bowling do
+
   context '#roll' do
-    # ...
+    bowling = Bowling.new
+    it "expect frames with [[2]]" do
+      bowling.roll(2)
+      expect(bowling.frames).to eq [[2]]
+    end
+
+    it "expect array with [[2,4]]" do
+      bowling.roll(4)
+      expect(bowling.frames).to eq [[2,4]]
+    end
+
+    it "expect array with [[2,4],[10,0]]" do
+      bowling.roll(10)
+      expect(bowling.frames).to eq [[2,4],[10,0]]
+    end
   end
 
   context '#score' do
-    # ...
+    bowling = Bowling.new
+    it "expect score 0" do
+      expect(bowling.score).to eq 0
+    end
+
+    it "expect score 2" do
+      bowling.roll(2)
+      expect(bowling.score).to eq 2
+    end
+
+    it "expect score 2" do
+      bowling.roll(2)
+      expect(bowling.score).to eq 4
+    end
+
+    it"strike expect score 24" do
+      bowling.roll(10)
+      bowling.roll(2)
+      bowling.roll(3)
+      expect(bowling.score).to eq 24
+    end
+
+    it"spare expect score 40" do
+      bowling.roll(5)
+      bowling.roll(5)
+      bowling.roll(3)
+      expect(bowling.score).to eq 40
+    end
+
   end
 end
