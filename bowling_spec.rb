d8dbef6dd8262c57bbb293932225bee8bd637c2f
nunodio <nunossdionisio@gmail.com>
2015-03-08 18:11:24 +0000

    Bowling Specifications - Version 1.0

--- bowling_spec.rb
+++ bowling_spec.rb
@@ -2,9 +2,34 @@ require_relative './bowling'
 require 'rspec'
 
 RSpec.describe Bowling do
-  # context '#roll' do
-  #   # ...
-  # end
+  context '#roll' do
+    it "Force play more than the rolls available." do
+      game = Bowling.new
+      20.times {game.roll(0)}
+      expect(game.roll(0)).to match /Game is already over/
+    end
+
+    it "Tries to insert invalid numbers to game." do
+      game = Bowling.new
+      expect(game.roll(-1)).to match /Invalid Values/
+    end
+
+    it "Tries to insert more than 10 pins in a normal frame." do
+      game = Bowling.new
+      # First Roll: 6 pins + Second Roll: 7 pins == 13 Pins => expected an error
+      game.roll(6)
+      expect(game.roll(7)).to match /Exceeds the maximum/
+    end
+
+    it "Tries to insert more than 10 pins in two rolls in Last frame, when dont Strike in first roll of the last frame." do
+      game = Bowling.new
+      9.times { game.roll(10) } # first 9 frames with 10
+      # Last Frame First Roll: 7 pins + Second Roll: 7 pins == 14 Pins => expected an error
+      game.roll(7)
+      expect(game.roll(7)).to match /Exceeds the maximum pins available/
+    end
+
+  end
 
   context '#score' do
     # Ideas taken from:
