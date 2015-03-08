c9cbf3f728e884b36e06d5b3ad7c40906d3e2503
KelyanneRibeiro <kelyanne.thome@gmail.com>
2015-03-08 18:54:35 +0000

    Update bowling_spec.rb

--- bowling_spec.rb
+++ bowling_spec.rb
@@ -3,10 +3,75 @@ require 'rspec'
 
 RSpec.describe Bowling do
   context '#roll' do
-    # ...
   end
 
   context '#score' do
-    # ...
+  	  		it 'pins.jpg' do
+				b = Bowling.new
+				b.roll(1)
+				b.roll(4)
+
+				b.roll(4)
+				b.roll(5)
+
+				b.roll(6)
+				b.roll(4)
+
+				b.roll(5)
+				b.roll(5)
+
+				b.roll(10)
+
+				b.roll(0)
+				b.roll(1)
+
+				b.roll(7)
+				b.roll(3)
+
+				b.roll(6)
+				b.roll(4)
+
+				b.roll(10)
+
+				b.roll(2)
+				b.roll(8)
+
+				b.roll(6)
+		    expect(b.score).to eq 133
+		end 
+
+		it 'strikes' do
+			b = Bowling.new
+			b.roll(10)
+			b.roll (10)
+			b.roll(10)
+			b.roll (10)
+			b.roll(10)
+			b.roll (10)
+			b.roll(10)
+			b.roll (10)
+			b.roll(10)
+			b.roll (10)
+			b.roll(10)
+			b.roll (10)
+			expect(b.score).to eq 300
+		end
+
+		it 'spares' do
+			b = Bowling.new
+			b.roll(5); b.roll(5)
+			b.roll (5); b.roll(5)
+			b.roll(5); b.roll(5)
+			b.roll (5); b.roll(5)
+			b.roll(5); b.roll(5)
+			b.roll (5); b.roll(5)
+			b.roll(5); b.roll(5)
+			b.roll (5); b.roll(5)
+			b.roll(5); b.roll(5)
+			b.roll (5); b.roll(5)
+			b.roll(5); b.roll(5)
+			b.roll (5)
+			expect(b.score).to eq 150
+		end
   end
 end
