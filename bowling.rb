51a8f8f86f349b640c8d5f170af2710cdc8a72e2
nunomsantos <nuno21nt@gmail.com>
2015-03-08 20:10:11 +0000

    Added some functions to simplify #score

--- bowling.rb
+++ bowling.rb
@@ -20,7 +20,7 @@ class Bowling
         playing_roll.odd? ? @pins_standing = 10 : @pins_standing -= pins
       end
     else  # tenth frame
-      rolls[20] = 0 if playing_roll > 18 && !(strike?(18) || spare?(18))
+      rolls[20] = 0 if playing_roll > 18 && !(strike?(9) || spare?(9))
       pins == pins_standing ? @pins_standing = 10 : @pins_standing -= pins
     end
     @playing_roll += 1
@@ -28,16 +28,14 @@ class Bowling
 
   def score
     points = 0
-    roll_counter = 0
     for frame in 0..9
-      if strike? (roll_counter)
-        points += 10 + bonus_strike(roll_counter)
-      elsif spare? (roll_counter)
-        points += 10 + rolls[roll_counter+2].to_i
+      if strike? (frame)
+        points += 10 + bonus_strike(frame)
+      elsif spare? (frame)
+        points += 10 + bonus_spare(frame)
       else
-        points += rolls[roll_counter].to_i + rolls[roll_counter+1].to_i
+        points += frame_points(frame)
       end
-      roll_counter += 2
     end
     points
   end
@@ -49,27 +47,35 @@ class Bowling
 
   private
 
-  def bonus_strike(roll_counter)
-    if roll_counter < 18
-      rolls[roll_counter+2].to_i +  if strike? (roll_counter+2)
-                                      rolls[roll_counter+4].to_i
-                                    else
-                                      rolls[roll_counter+3].to_i
-                                    end
+  def bonus_spare(frame)
+    rolls[2*frame+2].to_i
+  end
+
+  def bonus_strike(frame)
+    if frame < 9
+      rolls[2*frame+2].to_i + if strike? (frame+1)
+                                rolls[2*frame+4].to_i
+                              else
+                                rolls[2*frame+3].to_i
+                              end
     else  # tenth frame
-      rolls[roll_counter+1].to_i + rolls[roll_counter+2].to_i
+      rolls[2*frame+1].to_i + rolls[2*frame+2].to_i
     end
+  end
+
+  def frame_points(frame)
+    rolls[2*frame].to_i + rolls[2*frame +1].to_i
   end
 
   def game_over?
     rolls.find_index(nil) == nil
   end
 
-  def spare? (roll_counter)
-    rolls[roll_counter].to_i + rolls[roll_counter+1].to_i == 10
+  def spare? (frame)
+    rolls[2*frame].to_i + rolls[2*frame+1].to_i == 10
   end
 
-  def strike? (roll_counter)
-    rolls[roll_counter].to_i == 10
+  def strike? (frame)
+    rolls[2*frame].to_i == 10
   end
 end
